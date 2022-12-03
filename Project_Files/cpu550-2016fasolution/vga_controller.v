0e08aa23d07dd4116d9715ded4701f2da1793af2
astrosike <astrosike>
2022-12-03 17:48:21 +0800

    score printer

--- Project_Files/cpu550-2016fasolution/vga_controller.v
+++ Project_Files/cpu550-2016fasolution/vga_controller.v
@@ -20,18 +20,12 @@ output [7:0] g_data;
 output [7:0] r_data;                        
 ///////// ////                     
 reg [18:0] ADDR;
-reg [23:0] bgr_data;
+wire [23:0] bgr_data;
 wire VGA_CLK_n;
 wire [7:0] index;
 wire [23:0] bgr_data_raw;
 wire cBLANK_n,cHS,cVS,rst;
 
-initial
-begin
-x_s <= 0;
-y_s <= 0;
-counter <= 0;
-end
 ////
 assign rst = ~iRST_n;
 video_sync_generator LTM_ins (.vga_clk(iVGA_CLK),
@@ -72,44 +66,22 @@ img_index	img_index_inst (
 	.q ( bgr_data_raw)
 	);	
 
-	
-//////
-//always@(posedge VGA_CLK_n)
-//begin
-//	if (grid_data == 2000'h8)
-//	begin
-//	bgr_data <= 24'h00ffff;
-//	end
-//	else 
-//	begin
-//	bgr_data <= bgr_data_raw;
-//	end
-//end
-
-// square coordination
-reg [10:0] sqX;
-reg [10:0] sqY;
-// background coordination
+// print blocks and scores
+reg[9:0] score;// input
 reg [10:0] bgX;
 reg [10:0] bgY;
-reg [16:0] counter;
-// button
-input moveUp, moveDown, moveLeft, moveRight;
-reg[9:0] scores;// input
-integer SD,TD,HD
 
 always@(posedge VGA_CLK_n) begin
-	begin
-		
-	end
+	bgX <= ADDR % 10'd640;
+	bgY <= ADDR / 10'd640;
+	score <= 888;
+	
 end
-
-
-
+score_printer score_ind (score, bgX, bgY, bgr_data_raw, bgr_data);
 //////latch valid data at falling edge;
-assign b_data = bgr_data_raw[23:16];
-assign g_data = bgr_data_raw[15:8];
-assign r_data = bgr_data_raw[7:0]; 
+assign b_data = bgr_data[23:16];
+assign g_data = bgr_data[15:8];
+assign r_data = bgr_data[7:0]; 
 
 ///////////////////
 //////Delay the iHD, iVD,iDEN for one clock cycle;
@@ -121,8 +93,340 @@ begin
 end
 
 endmodule
- 	
 
+
+module score_printer(score, bgX, bgY, bgr_data_raw, bgr_data);
+	input [9:0] score;
+	input [10:0] bgX, bgY;
+	input [23:0] bgr_data_raw;
+	reg [6:0] numH, numT, numS;
+	output reg [23:0] bgr_data;
+	reg[23:0] valid_score_color;
+	reg[23:0] invalid_score_color;
+	reg h0,h1,h2,h3,h4,h5,h6;
+	reg t0,t1,t2,t3,t4,t5,t6;
+	reg s0,s1,s2,s3,s4,s5,s6;
+	reg [3:0] HD,SD,TD;	
+	always @(*) begin 
+		HD <= score / 100;
+		TD <= (score / 10) % 10;
+		SD <= score % 10;
+		invalid_score_color <= 24'h000000;
+		valid_score_color <= 24'hFFFFFF;
+		h0 <= (bgX >= 490) && (bgX <= 530) && (bgY >= 205) && (bgY<=215);
+		h1 <= (bgX >= 490) && (bgX <= 500) && (bgY >= 205) && (bgY<=245);
+		h2 <= (bgX >= 520) && (bgX <= 530) && (bgY >= 205) && (bgY<=245);
+		h3 <= (bgX >= 500) && (bgX <= 520) && (bgY >= 235) && (bgY<=245);
+		h4 <= (bgX >= 490) && (bgX <= 500) && (bgY >= 235) && (bgY<=275);
+		h5 <= (bgX >= 520) && (bgX <= 530) && (bgY >= 235) && (bgY<=275);
+		h6 <= (bgX >= 490) && (bgX <= 530) && (bgY >= 265) && (bgY<=275);
+
+		t0 <= (bgX >= 540) && (bgX <= 580) && (bgY >= 205) && (bgY<=215);
+		t1 <= (bgX >= 540) && (bgX <= 550) && (bgY >= 205) && (bgY<=245);
+		t2 <= (bgX >= 570) && (bgX <= 580) && (bgY >= 205) && (bgY<=245);
+		t3 <= (bgX >= 550) && (bgX <= 570) && (bgY >= 235) && (bgY<=245);
+		t4 <= (bgX >= 540) && (bgX <= 550) && (bgY >= 235) && (bgY<=275);
+		t5 <= (bgX >= 570) && (bgX <= 580) && (bgY >= 235) && (bgY<=275);
+		t6 <= (bgX >= 540) && (bgX <= 580) && (bgY >= 265) && (bgY<=275);
+
+		s0 <= (bgX >= 590) && (bgX <= 630) && (bgY >= 205) && (bgY<=215);
+		s1 <= (bgX >= 590) && (bgX <= 600) && (bgY >= 205) && (bgY<=245);
+		s2 <= (bgX >= 620) && (bgX <= 630) && (bgY >= 205) && (bgY<=245);
+		s3 <= (bgX >= 600) && (bgX <= 620) && (bgY >= 235) && (bgY<=245);
+		s4 <= (bgX >= 590) && (bgX <= 600) && (bgY >= 235) && (bgY<=275);
+		s5 <= (bgX >= 620) && (bgX <= 630) && (bgY >= 235) && (bgY<=275);
+		s6 <= (bgX >= 590) && (bgX <= 630) && (bgY >= 265) && (bgY<=275);
+		
+		case (HD)
+			0: begin
+					numH[0] <= 1;
+					numH[1] <= 1;
+					numH[2] <= 1;
+					numH[3] <= 0;
+					numH[4] <= 1;
+					numH[5] <= 1;
+					numH[6] <= 1;
+				end
+			1: begin
+					numH[0] <= 0;
+					numH[1] <= 0;
+					numH[2] <= 1;
+					numH[3] <= 0;
+					numH[4] <= 0;
+					numH[5] <= 1;
+					numH[6] <= 0;
+				end
+			2: begin
+					numH[0] <= 1;
+					numH[1] <= 0;
+					numH[2] <= 1;
+					numH[3] <= 1;
+					numH[4] <= 1;
+					numH[5] <= 0;
+					numH[6] <= 1;
+				end
+			3: begin
+					numH[0] <= 1;
+					numH[1] <= 0;
+					numH[2] <= 1;
+					numH[3] <= 1;
+					numH[4] <= 0;
+					numH[5] <= 1;
+					numH[6] <= 1;
+				end
+			4: begin
+					numH[0] <= 0;
+					numH[1] <= 1;
+					numH[2] <= 1;
+					numH[3] <= 1;
+					numH[4] <= 0;
+					numH[5] <= 1;
+					numH[6] <= 0;
+				end
+			5: begin
+					numH[0] <= 1;
+					numH[1] <= 1;
+					numH[2] <= 0;
+					numH[3] <= 1;
+					numH[4] <= 0;
+					numH[5] <= 1;
+					numH[6] <= 1;
+				end
+			6: begin
+					numH[0] <= 1;
+					numH[1] <= 1;
+					numH[2] <= 0;
+					numH[3] <= 1;
+					numH[4] <= 1;
+					numH[5] <= 1;
+					numH[6] <= 1;
+				end
+			7: begin
+					numH[0] <= 1;
+					numH[1] <= 0;
+					numH[2] <= 1;
+					numH[3] <= 0;
+					numH[4] <= 0;
+					numH[5] <= 1;
+					numH[6] <= 0;
+				end
+			8: begin
+					numH[0] <= 1;
+					numH[1] <= 1;
+					numH[2] <= 1;
+					numH[3] <= 1;
+					numH[4] <= 1;
+					numH[5] <= 1;
+					numH[6] <= 1;
+				end
+			9: begin
+					numH[0] <= 1;
+					numH[1] <= 1;
+					numH[2] <= 1;
+					numH[3] <= 1;
+					numH[4] <= 0;
+					numH[5] <= 1;
+					numH[6] <= 0;
+				end
+		endcase
+
+		case (TD)
+			0: begin
+					numT[0] <= 1;
+					numT[1] <= 1;
+					numT[2] <= 1;
+					numT[3] <= 0;
+					numT[4] <= 1;
+					numT[5] <= 1;
+					numT[6] <= 1;
+				end
+			1: begin
+					numT[0] <= 0;
+					numT[1] <= 0;
+					numT[2] <= 1;
+					numT[3] <= 0;
+					numT[4] <= 0;
+					numT[5] <= 1;
+					numT[6] <= 0;
+				end
+			2: begin
+					numT[0] <= 1;
+					numT[1] <= 0;
+					numT[2] <= 1;
+					numT[3] <= 1;
+					numT[4] <= 1;
+					numT[5] <= 0;
+					numT[6] <= 1;
+				end
+			3: begin
+					numT[0] <= 1;
+					numT[1] <= 0;
+					numT[2] <= 1;
+					numT[3] <= 1;
+					numT[4] <= 0;
+					numT[5] <= 1;
+					numT[6] <= 1;
+				end
+			4: begin
+					numT[0] <= 0;
+					numT[1] <= 1;
+					numT[2] <= 1;
+					numT[3] <= 1;
+					numT[4] <= 0;
+					numT[5] <= 1;
+					numT[6] <= 0;
+				end
+			5: begin
+					numT[0] <= 1;
+					numT[1] <= 1;
+					numT[2] <= 0;
+					numT[3] <= 1;
+					numT[4] <= 0;
+					numT[5] <= 1;
+					numT[6] <= 1;
+				end
+			6: begin
+					numT[0] <= 1;
+					numT[1] <= 1;
+					numT[2] <= 0;
+					numT[3] <= 1;
+					numT[4] <= 1;
+					numT[5] <= 1;
+					numT[6] <= 1;
+				end
+			7: begin
+					numT[0] <= 1;
+					numT[1] <= 0;
+					numT[2] <= 1;
+					numT[3] <= 0;
+					numT[4] <= 0;
+					numT[5] <= 1;
+					numT[6] <= 0;
+				end
+			8: begin
+					numT[0] <= 1;
+					numT[1] <= 1;
+					numT[2] <= 1;
+					numT[3] <= 1;
+					numT[4] <= 1;
+					numT[5] <= 1;
+					numT[6] <= 1;
+				end
+			9: begin
+					numT[0] <= 1;
+					numT[1] <= 1;
+					numT[2] <= 1;
+					numT[3] <= 1;
+					numT[4] <= 0;
+					numT[5] <= 1;
+					numT[6] <= 0;
+				end
+		endcase
+
+		case (SD)
+			0: begin
+					numS[0] <= 1;
+					numS[1] <= 1;
+					numS[2] <= 1;
+					numS[3] <= 0;
+					numS[4] <= 1;
+					numS[5] <= 1;
+					numS[6] <= 1;
+				end
+			1: begin
+					numS[0] <= 0;
+					numS[1] <= 0;
+					numS[2] <= 1;
+					numS[3] <= 0;
+					numS[4] <= 0;
+					numS[5] <= 1;
+					numS[6] <= 0;
+				end
+			2: begin
+					numS[0] <= 1;
+					numS[1] <= 0;
+					numS[2] <= 1;
+					numS[3] <= 1;
+					numS[4] <= 1;
+					numS[5] <= 0;
+					numS[6] <= 1;
+				end
+			3: begin
+					numS[0] <= 1;
+					numS[1] <= 0;
+					numS[2] <= 1;
+					numS[3] <= 1;
+					numS[4] <= 0;
+					numS[5] <= 1;
+					numS[6] <= 1;
+				end
+			4: begin
+					numS[0] <= 0;
+					numS[1] <= 1;
+					numS[2] <= 1;
+					numS[3] <= 1;
+					numS[4] <= 0;
+					numS[5] <= 1;
+					numS[6] <= 0;
+				end
+			5: begin
+					numS[0] <= 1;
+					numS[1] <= 1;
+					numS[2] <= 0;
+					numS[3] <= 1;
+					numS[4] <= 0;
+					numS[5] <= 1;
+					numS[6] <= 1;
+				end
+			6: begin
+					numS[0] <= 1;
+					numS[1] <= 1;
+					numS[2] <= 0;
+					numS[3] <= 1;
+					numS[4] <= 1;
+					numS[5] <= 1;
+					numS[6] <= 1;
+				end
+			7: begin
+					numS[0] <= 1;
+					numS[1] <= 0;
+					numS[2] <= 1;
+					numS[3] <= 0;
+					numS[4] <= 0;
+					numS[5] <= 1;
+					numS[6] <= 0;
+				end
+			8: begin
+					numS[0] <= 1;
+					numS[1] <= 1;
+					numS[2] <= 1;
+					numS[3] <= 1;
+					numS[4] <= 1;
+					numS[5] <= 1;
+					numS[6] <= 1;
+				end
+			9: begin
+					numS[0] <= 1;
+					numS[1] <= 1;
+					numS[2] <= 1;
+					numS[3] <= 1;
+					numS[4] <= 0;
+					numS[5] <= 1;
+					numS[6] <= 0;
+				end
+		endcase
+		if ((h0 && numH[0]) || (h1 && numH[1]) || (h2 && numH[2]) || (h3 && numH[3]) || (h4 && numH[4]) || (h5 && numH[5]) || (h6 && numH[6]) ||
+			 (t0 && numT[0]) || (t1 && numT[1]) || (t2 && numT[2]) || (t3 && numT[3]) || (t4 && numT[4]) || (t5 && numT[5]) || (t6 && numT[6]) ||
+			 (s0 && numS[0]) || (s1 && numS[1]) || (s2 && numS[2]) || (s3 && numS[3]) || (s4 && numS[4]) || (s5 && numS[5]) || (s6 && numS[6])) begin
+			bgr_data <= valid_score_color;
+//		end else if (h0 || h1 || h2 || h3 || h4 || h5 || h6) begin
+//			bgr_data <= invalid_score_color;
+		end else begin
+		   bgr_data <= bgr_data_raw;
+		end		
+	end
+	
+endmodule
 
 
 
